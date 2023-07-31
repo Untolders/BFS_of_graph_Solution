49e605059a3947e355e2a9e4b6812fae278008af
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-07-31 15:08:03 +0530

    Create BFS_of_graph_Solution.cpp

---
+++ BFS_of_graph_Solution.cpp
@@ -0,0 +1,110 @@
+***********************************************************************  Question  ****************************************************************************************
+
+Given a directed graph. The task is to do Breadth First Traversal of this graph starting from 0.
+Note: One can move from node u to node v only if there's an edge from u to v. Find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the input graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
+
+
+Example 1:
+
+Input:
+
+Output: 0 1 2 3 4
+Explanation: 
+0 is connected to 1 , 2 , 3.
+2 is connected to 4.
+so starting from 0, it will go to 1 then 2
+then 3. After this 2 to 4, thus bfs will be
+0 1 2 3 4.
+Example 2:
+
+Input:
+
+Output: 0 1 2
+Explanation:
+0 is connected to 1 , 2.
+so starting from 0, it will go to 1 then 2,
+thus bfs will be 0 1 2. 
+
+Your task:
+You dont need to read input or print anything. Your task is to complete the function bfsOfGraph() which takes the integer V denoting the number of vertices and adjacency list as input parameters and returns  a list containing the BFS traversal of the graph starting from the 0th vertex from left to right.
+
+
+Expected Time Complexity: O(V + E)
+Expected Auxiliary Space: O(V)
+
+
+Constraints:
+1 ≤ V, E ≤ 104
+  ***********************************************************************  Solution  ****************************************************************************************
+
+  //{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int N, vector<int> g[]) {
+        // Code here
+    queue<int> qu;
+    vector<int> res;
+    // Create vector of N and set all of the them as false
+    vector<bool> vis(N, false);
+    // N is the root: starting point
+    qu.push(0);
+    // Set first root as visited
+    vis[0]=true;
+    // While queue q is not empty:
+    while(!qu.empty())
+    {
+        // Deque vertex from queue
+        int num=qu.front();
+        qu.pop();
+        // Enque all not yet visited adjancent
+        res.push_back(num);
+        for(auto it=g[num].begin(); it!=g[num].end();it++)
+        {
+            if(vis[*it]==false)
+            {
+                vis[*it]=true;
+                qu.push(*it);
+            }
+        }
+    }
+    return res;
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+  
